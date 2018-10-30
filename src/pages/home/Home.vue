1a1ccce1259a8fda034e6a326767756c93ecdcf8
matengfei <matengfei@imzhiliao.com>
2018-10-30 17:28:02 +0800

    vue_pro_01

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,69 @@
+<template>
+<div>
+   <home-header></home-header>
+   <home-swiper :list="swiperList"></home-swiper>
+   <home-icons :list="iconsList"></home-icons>
+   <home-recommend :list="recommendList"></home-recommend>
+   <home-weekend :list="weekendList"></home-weekend>
+</div>
+</template>
+<script>
+import HomeHeader from './components/header'
+import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weelend'
+import axios from 'axios'
+import { mapState } from 'vuex'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
+  },
+  data () {
+    return {
+      lastCity: '',
+      swiperList: [],
+      iconsList: [],
+      recommendList: [],
+      weekendList: []
+    }
+  },
+  computed: {
+    ...mapState(['city'])
+  },
+  mounted () {
+    this.lastCity = this.city
+    this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json?city=' + this.city)
+        .then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.swiperList = data.swiperList
+        this.iconsList = data.iconList
+        this.recommendList = data.recommendList
+        this.weekendList = data.weekendList
+      }
+      console.log(res)
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
